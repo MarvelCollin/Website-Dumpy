949ef0ab6d12e1b4d9ad5f6c8c7a1e54485b1eec
Marvel Collin <marvelcollin7@gmail.com>
2024-02-21 23:11:57 +0700

    fix: fix js

--- Carousel2/script.js
+++ Carousel2/script.js
@@ -1,6 +1,15 @@
+// declare untuk set point awal dari index carousel
 let currentSlide = 0;
+
+// ambil semua .carousel.items, kita masukan kedalam array images
 const images = document.querySelectorAll(".carousel-items");
 
+// length ini untuk membaca seberapa banyak .carousel-items
+// kita ada di html
+const length = images.length;
+
+// disini adalah function untuk hide semua images dan cuman 
+// show 1 saja (bagian current index)
 function hidden(){
     images.forEach(element => {
         element.style.display = 'none';
@@ -9,11 +18,11 @@ function hidden(){
 }
 
 function nextSlide(){
-    currentSlide++;
+    currentSlide = (currentSlide + 1) % length;
     hidden();
 }
 
 function prevSlide(){
-    currentSlide--;
+    currentSlide = (currentSlide - 1 + length) % length;
     hidden();
 }
